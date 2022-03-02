b4e370154f96ed7577a78edc3ee497bd93fb7e43
Brandon <hongbra@oregonstate.edu>
2022-03-02 15:12:18 -0800

    added a title

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var handlebars = require('express-handlebars').create({
 app.engine('handlebars', handlebars.engine);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
-// app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'handlebars');
 app.set('mysql', mysql);
 app.use('/', express.static('public'));
@@ -25,29 +24,6 @@ app.use('/pokedex.handlebars', require('./public/js/pokedex.js'));
 app.use('/pokemon.handlebars',require('./public/js/pokemon.js'));
 app.use('/pokedex_pokemon.handlebars',require('./public/js/pokedex_pokemon.js'));
 
-
-// app.get('/',function(req,res){
-//     res.render('index.handlebars') //We can omit the .handlebars extension as we do below
-// });
-
-// app.get('/pokedex.handlebars',function(req,res){
-//     res.render('pokedex.handlebars') //We can omit the .handlebars extension as we do below
-// });
-
-// app.get('/pokemon.handlebars',function(req,res){
-//     res.render('pokemon.handlebars') //We can omit the .handlebars extension as we do below
-// });
-
-// app.get('/regions.handlebars',function(req,res){
-//     res.render('regions.handlebars') //We can omit the .handlebars extension as we do below
-// });
-
-// app.get('/trainer.handlebars',function(req,res){
-//     res.render('trainer.handlebars') //We can omit the .handlebars extension as we do below
-// });
-
-// app.use('/pokedex', require('./pokedex.js'));
-// app.use('/planets', require('./planets.js'));
 
 app.use(function(req,res){
     res.status(404);
